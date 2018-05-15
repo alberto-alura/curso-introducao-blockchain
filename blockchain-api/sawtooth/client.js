42a1bb64ff34392fb579d6269d1384aef37583c5
Alura Verde <aluraverde@Alura-Verde.home>
2018-05-15 17:06:18 -0300

    aula5.3 enviando informacoes para a api do sawtooth

--- blockchain-api/sawtooth/client.js
+++ blockchain-api/sawtooth/client.js
@@ -5,6 +5,12 @@ const {buildSawtoothPackage,sendToSawtoothApi} = require('./infra');
 
 const registerBlockchain = (payload) => {
   console.log(payload);
+
+  const payloadBytes = cbor.encode(JSON.stringify(payload));
+
+  const batchBytes = buildSawtoothPackage(payloadBytes,payload.userNumber);
+
+  sendToSawtoothApi(batchBytes);
 }
 
 module.exports = { registerBlockchain }
